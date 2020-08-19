b980658dc18a33e0fc67a00b74abacd3edc3b3f6
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-19 12:02:16 -0700

    Implemented functional instantiation of stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // Use a variable to keep track of size
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // when pushing, increment size and create the new key/value pair
+    size += 1;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    // when pop, decrement size unless < 0 and return the deleted item
+    let temp = storage[size];
+    delete storage[size];
+    if (size > 0) {
+      size -= 1;
+    }
+    return temp;
   };
 
   someInstance.size = function() {
+    // can return the variable size
+    return size;
   };
 
   return someInstance;
