4ebd5769c9f8f8df54d29d53014df27030666a9d
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-19 14:58:13 -0700

    Implemented stack and queue with prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.front = 0;
+  instance.back = 0;
+
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(string) {
+  this.storage[this.back] = string;
+  this.back += 1;
+};
+
+queueMethods.dequeue = function(string) {
+  if (this.size() > 0) {
+    let temp = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front += 1;
+    return temp;
+  }
+};
+
+queueMethods.size = function(string) {
+  return this.back - this.front;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+
+  instance.storage = {};
+  instance.stacksize = 0;
+
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(string) {
+  this.storage[this.stacksize] = string;
+  this.stacksize += 1;
+};
+
+stackMethods.pop = function() {
+  if (this.stacksize > 0) {
+    this.stacksize -= 1;
+    let temp = this.storage[this.stacksize];
+    delete this.storage[this.stacksize];
+    return temp;
+  }
+};
+
+stackMethods.size = function() {
+  return this.stacksize;
+};
 
