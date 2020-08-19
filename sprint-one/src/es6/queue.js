d609e51dad661edf2f13c28fda78196547d1ff65
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-19 15:11:30 -0700

    Implemented stack and queue in the es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.front = 0;
+    this.back = 0;
+  }
+
+  enqueue(string) {
+    this.storage[this.back] = string;
+    this.back += 1;
+  }
+
+  dequeue() {
+    if (this.size() > 0) {
+      let temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front += 1;
+      return temp;
+    }
+  }
+
+  size() {
+    return this.back - this.front;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.stacksize = 0;
+  }
+  push(string) {
+    this.storage[this.size()] = string;
+    this.stacksize += 1;
   }
 
+  pop() {
+    if (this.size() > 0) {
+      this.stacksize -= 1;
+      let temp = this.storage[this.size()];
+      delete this.storage[this.size()];
+      return temp;
+    }
+  }
+
+  size() {
+    return this.stacksize;
+  }
 }
