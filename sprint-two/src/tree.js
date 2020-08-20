1aad5a78b8e9372d1ea38e60792f19fa3c49bd07
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-20 11:09:04 -0700

    Implemented the basic tree class with add and contains functions using a functional-shared methods style

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,50 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  // extend treeMethods into newTree objects
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
+// input: takes in a value to be set as the value of a new node
+// output: updated tree with the new node within it
+// constraint: n/a?
+// edge cases: same process for adding into any tree or subtree
 treeMethods.addChild = function(value) {
+  // create a new node (a subtree) with target value
+  var node = Tree(value);
+  // add the node as a child of the tree
+  this.children.push(node);
 };
 
+// input: takes in any target value
+// output: boolean whether or not it is found within the tree
+// constraint: n/a?
+// edge cases: should return false if nothing is in the tree
 treeMethods.contains = function(target) {
+  // traverse through the tree and its children starting at the root
+  var node = this.children;
+  // keep track of whether or not it has been found yet
+  var found = false;
+  for (let i = 0; i < node.length; i++) {
+    // if the target has been found or the current node is a match, return true
+    if (node[i].value === target || found === true) {
+      return true;
+    } else if (node[i].children.length > 0) {
+      // if the current tree has children, recurse through each of the children
+      found = node[i].contains(target);
+    }
+  }
+  // return false if none is found
+  return found;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild should run in O(log n) time if it is a binary search tree and the contains method should run in linear O(n) time
  */
