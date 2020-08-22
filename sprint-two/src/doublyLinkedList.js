12ebc5f19daf3d8bb96192923e772c45ddc1cee9
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-22 10:21:25 -0700

    Added the doublyLinkedList spec/src files and created the initial template/tests

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,61 @@
+var DoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  // I:
+  // O:
+  // C:
+  // E:
+  list.addToHead = function(value) {
+
+  };
+
+  // I:
+  // O:
+  // C:
+  // E:
+  list.addToTail = function(value) {
+
+  };
+
+  // I:
+  // O:
+  // C:
+  // E:
+  list.removeHead = function() {
+
+  };
+
+  // I:
+  // O:
+  // C:
+  // E:
+  list.removeTail = function() {
+
+  };
+
+  // I:
+  // O:
+  // C:
+  // E:
+  list.contains = function(target) {
+
+  };
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.prev = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
